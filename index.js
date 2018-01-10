7d5fc7399b482537532c04ace5bc4b9841391a4c
Igor Belogurov <belogurovigor@MacBook-Pro-Igor.local>
2018-01-10 18:12:38 +0300

    добавил мидлвару ошибки

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const logger = require('morgan')
 
 const config = require('./config')
-const routers = require('./routers');
+const { error } = require('./middleware')
+const routers = require('./routers')
 
 const app = express()
 
-app.use(express.static(config.paths.public));
+app.use(express.static(config.paths.public))
 app.set('config', config);
 
 app.use(logger('dev'))
@@ -14,6 +15,8 @@ app.use(logger('dev'))
 app.use('/', routers.main)
 app.use('/products', routers.product)
 app.use('/search', routers.search)
+
+app.use(error.notFound);
 
 app.listen(config.port, () => console.log(`Server is running on: http://localhost:${config.port}`))
 
